f57b72261adf8b8d75d847945c1627624d34a87f
Yash Gaitonde <gaitoyas20@gmail.com>
2020-05-25 16:05:06 -0400

    broken cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -34,45 +34,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 app.use(session({
-  name:'session-id',
+  name: 'session-id',
   secret: '12345-67890-09876-54321',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
-}))
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next){
-  console.log(req.session);
+  console.log(req.session.user);
   if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    if(!authHeader) {
       var err = new Error("You are not authenticated")
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401
       return next(err)
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-    var username = auth[0]
-    var password = auth[1]
-    if (username === 'admin' && password === 'password') {
-      req.session.user == 'admin'
+  }
+  else{
+    if(req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error("You are not authenticated")
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401
-      return next(err)
-    }
-  }
-  else {
-    if(req.session.user === 'admin') {
-      next();
-    }
-    else{
-      var err = new Error("You are not authenticated")
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401
+      err.status = 403
       return next(err)
     }
   }
@@ -82,8 +67,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
